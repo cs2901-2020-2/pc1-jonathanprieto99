fb8e1ed90fd66ec68c40513fc729e2c7041e1500
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-08 10:05:05 -0500

    First Test

--- src/main/java/lab/IniciarSesion.java
+++ src/main/java/lab/IniciarSesion.java
@@ -1,9 +1,14 @@
 package lab;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.logging.Logger;
 
 public class IniciarSesion{
+
+    static final Logger logger = Logger.getLogger(IngresarData.class.getName());
+
     public static RegistrApp readUser() throws Exception {
+        logger.info("Ingresar nombre: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name = reader.readLine();
         if (name.isEmpty()){
