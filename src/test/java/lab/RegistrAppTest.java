ca0355e58453fe7308bee397bb30172c7429fb35
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-08 09:55:21 -0500

    First Test

---
+++ src/test/java/lab/RegistrAppTest.java
@@ -0,0 +1,21 @@
+package lab;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class EstacionMeteorologicaTest {
+    @Test
+    public void test_get_data_single() throws Exception {
+
+        RegistrApp mainApp = IniciarSesion.readUser();
+
+        Cliente nuevoCliente = new Cliente(0);
+
+        mainApp.addObserver(nuevoCliente);
+
+        IngresarData.readInput(mainApp);
+
+        Assert.assertEquals(nuevoCliente.getData(), mainApp.getData());
+    }
+}
